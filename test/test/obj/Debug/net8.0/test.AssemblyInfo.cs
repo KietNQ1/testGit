aacd8ba57432cd1827e1745f5dfae5f9a39a0819
Tran Thi Ngoc Vy <meotraan0703@gmail.com>
2025-06-10 20:32:54 +0700

    Vy

--- test/test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ test/test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b893b074804e8edb89cc8d4ec8e93a5a0dadc554")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
